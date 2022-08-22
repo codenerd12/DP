42397650b66e7de617966e48559f46635ba4cb72
manishbohra <manishbohra@IN-5FM7473.Nagarro.local>
2022-08-22 23:32:48 +0530

    Optimize from 3D array to 2D array

--- src/com/problems/DP38BuyAndSell4.java
+++ src/com/problems/DP38BuyAndSell4.java
@@ -4,62 +4,61 @@ import java.util.Arrays;
 
 public class DP38BuyAndSell4 {
 
-	public static int maxProfit(int[] prices, int ind, int buy, int k, int n, int[][][] dp) {
+	public static int maxProfit(int[] prices, int ind, int transNo, int k, int n, int[][] dp) {
         
-        if(ind==n || k==0)
+        if(ind==n || transNo == 2*k)
             return 0;
         
-        if(dp[ind][buy][k]!=-1)
-            return dp[ind][buy][k];
+        if(dp[ind][transNo]!=-1)
+            return dp[ind][transNo];
         
         int pick = 0;
         int nopick = 0;
-        if(buy==1) {
-            pick = -prices[ind]+maxProfit(prices, ind+1, 0, k, n, dp);
-            nopick = maxProfit(prices, ind+1, 1, k, n, dp);
+        if(transNo%2==0) {
+            pick = -prices[ind]+maxProfit(prices, ind+1, transNo+1, k, n, dp);
+            nopick = maxProfit(prices, ind+1, transNo, k, n, dp);
         } else {
-            pick = prices[ind]+maxProfit(prices, ind+1, 1, k-1, n, dp);
-            nopick = maxProfit(prices, ind+1, 0, k, n, dp);
+            pick = prices[ind]+maxProfit(prices, ind+1, transNo+1, k, n, dp);
+            nopick = maxProfit(prices, ind+1, transNo, k, n, dp);
         }
         
-        dp[ind][buy][k] = Math.max(pick, nopick);
+        dp[ind][transNo] = Math.max(pick, nopick);
         
-        return dp[ind][buy][k];
+        return dp[ind][transNo];
     }
 	public static void main(String[] args) {
 		
 		int[] prices = {3, 2, 6, 5, 0, 3};
 		int n = prices.length;
 		int k = 2;
-		/*int[][][] dp = new int[n+1][2][k+1];
-        for(int[][] rows: dp)
-           for(int[] row: rows)
-                Arrays.fill(row, -1);
-        return maxProfit(prices, 0, 1, k, n, dp);*/
+		/*int[][] dp = new int[n+1][2*k];
+        for(int[] row: dp)
+            Arrays.fill(row, -1);
+        //return maxProfit(prices, 0, 0, k, n, dp);
+        System.out.println(maxProfit(prices, 0, 0, k, n, dp));*/
 		
 		//----------Tabulation
-		 int[][][] dp = new int[n+1][2][k+1];
+		 int[][] dp = new int[n+1][2*k+1];
 	        
-	        for(int ind=n-1;ind>=0;ind--) {
-	            for(int buy=0;buy<=1;buy++) {
-	                for(int tran=1;tran<=k;tran++) {
+	        for(int ind = n-1;ind>=0;ind--) {
+	        	
+	        	for(int tran=2*k-1;tran>=0;tran--) {
 	                    
-	                    int pick = 0;
+	                	int pick = 0;
 	                    int nopick = 0;
-	                    if(buy==1) {
-	                        pick = -prices[ind]+dp[ind+1][0][tran];
-	                        nopick = dp[ind+1][1][tran];
+	                    if(tran%2==0) {
+	                        pick = -prices[ind]+dp[ind+1][tran+1];
+	                        nopick = dp[ind+1][tran];
 	                    } else {
-	                        pick = prices[ind]+dp[ind+1][1][tran-1];
-	                        nopick = dp[ind+1][0][tran];
+	                        pick = prices[ind]+dp[ind+1][tran+1];
+	                        nopick = dp[ind+1][tran];
 	                    }
-
-	                    dp[ind][buy][tran] = Math.max(pick, nopick);
-	                }
+	                    
+	                    dp[ind][tran] = Math.max(pick, nopick);
 	            }
 	        }
 	        
-	        System.out.println("Max Profit perform by K Transactions: "+dp[0][1][k]);
+	        System.out.println("Max Profit perform by K Transactions: "+dp[0][0]);
 		
 
 	}
